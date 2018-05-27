c5beee7286b51237e579db08b20eb6d3d1e85f93
FilDan <filip.danieluk94@gmail.com>
2018-05-27 21:59:16 +0200

    Creates compressed_data if necessary

--- download_data.py
+++ download_data.py
@@ -140,6 +140,9 @@ def main():
     links = get_download_links()
     links = stop_duplicated_downloads(links)
     if links:
+        if not os.path.exists('./compressed_data'):
+            os.makedirs('./compressed_data')
+            
         with ThreadPoolExecutor(max_workers=25) as executor:
             future_to_url = {executor.submit(download_file, link): link for link in links}
             for future in as_completed((future_to_url)):
