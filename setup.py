f2a54272412a5e5ee6cca954bc3e61bfa058f651
Filip Danieluk <filip.danieluk94@gmail.com>
2018-09-29 14:47:10 +0200

    new structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='deep_aqi',
+      version='0.0.1',
+      description='Project aiming to build air pollution predictive model',
+      url='https://github.com/imintogoodmusic/deep_aqi',
+      author='Filip Danieluk',
+      author_email='filip.danieluk94@gmail.com',
+      license='MIT',
+      packages=['deep_aqi.src', 'deep_aqi'],
+      zip_safe=False)
